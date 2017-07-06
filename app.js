9b8630fc1ab00cb16e443e921389ad25a842686f
KernelFreeze <kernelfreeze@outlook.com>
2017-07-06 12:49:53 -0500

    Mejorando debug

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = err;
+    res.locals.debug = req.app.get('env') === 'development';
 
     console.log(require('util').inspect(err));
 
